0341e21e673d469e779de4159108ce6b3ab17ebe
shokred <shokred@yandex.ru>
2021-03-02 23:39:05 +0700

    Add spring framework

---
+++ task/src/main/java/name/shokred/popugjira/PopugjiraApplication.java
@@ -0,0 +1,13 @@
+package name.shokred.popugjira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopugjiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopugjiraApplication.class, args);
+	}
+
+}
